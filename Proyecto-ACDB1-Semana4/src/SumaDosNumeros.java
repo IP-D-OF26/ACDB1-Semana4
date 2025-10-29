feb99a6fcdb18f33d13d3a47640e8b71113cca42
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2025-10-29 16:37:05 -0500

    subiendo la clase para sumar 2 numeros

--- Proyecto-ACDB1-Semana4/src/SumaDosNumeros.java
+++ Proyecto-ACDB1-Semana4/src/SumaDosNumeros.java
@@ -4,9 +4,25 @@
  * @author Daniel Irene
  * @version 1.0
  */
-
+import java.util.Scanner;
 public class SumaDosNumeros {
-    
+    public static void main(String[] args) {
+        //ENTRADA DE DATOS
+        //[Clase-Api-JDK-para-ingreso-datos: Scanner -> java.util] 
+        //[nombre-variable/obj-para-ingredo-datos: nombreVariable] 
+        //[=] inicializar el objeto/variable
+        //[Clase-Api-JDK-para-ingreso-datos: Scanner ] 
+        //[Fuento-del-ingreso-datos: System.in => las entradas del sistema]
+        Scanner teclado = new Scanner(System.in);
+        int num1, num2, respuesta;
+        System.out.println("Deme dos numeros: "); //Carpintaria => tunear
+        num1 = teclado.nextInt();
+        num2 = teclado.nextInt();
+        respuesta = num1 + num2;
+        System.out.println("La respuestas es: ");
+        System.out.println(respuesta);
+        
+    }
 }
 
 /**
